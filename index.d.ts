7a2d68793a1728243e75154866c7f488037edd69
Jdeshz <17858801330@163.com>
2019-09-09 15:16:29 +0800

    v2.0.7 类型声明文件更新

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+declare class ElementUIComponent extends Vue {
+  /** Install component into Vue */
+  static install(vue: typeof Vue): void
+}
+
+export declare class ElTableBar extends ElementUIComponent {
+  /** 自适应模式 */
+  fixed: boolean
+
+  /** 滚动条底部距离 */
+  bottom: number
+
+  /** 滚轮响应延迟 */
+  delay: number
+
+  /** 静态模式 */
+  static: boolean
+}
